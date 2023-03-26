8852295065c5e40b61e009581b5949e94d7ce5dd
Alvin <goape7914@gmail.com>
2023-03-26 15:39:52 +0800

    第一次修改完成

--- html/js/product-service.js
+++ html/js/product-service.js
@@ -650,3 +650,85 @@ $(document).ready(function() {
     sync15.data('owl.carousel').to(number, 300, true);
   });
 });
+
+// sync17 sync18
+$(document).ready(function() {
+    
+  var sync17 = $("#sync17");
+  var sync18 = $("#sync18");
+  var slidesPerPage = 4; //globaly define number of elements per page
+  var syncedSecondary = true;
+
+      sync17.owlCarousel({
+        items : 1,
+        slideSpeed : 2000,
+        nav: true,
+        autoplay: false,
+        dots: false,
+        loop: true,
+        responsiveRefreshRate : 200,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+      }).on('changed.owl.carousel', syncPosition);
+
+      sync18
+        .on('initialized.owl.carousel', function () {
+          sync18.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+        items : slidesPerPage,
+        dots: false,
+        nav: false,
+        margin:5,
+        smartSpeed: 200,
+        slideSpeed : 500,
+        slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+        responsiveRefreshRate : 100
+      }).on('changed.owl.carousel', syncPosition2);
+
+  function syncPosition(el) {
+    //if you set loop to false, you have to restore this next line
+    //var current = el.item.index;
+    
+    //if you disable loop you have to comment this block
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    
+    if(current < 0) {
+      current = count;
+    }
+    if(current > count) {
+      current = 0;
+    }
+    
+    //end block
+
+    sync18
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = sync18.find('.owl-item.active').length - 1;
+    var start = sync18.find('.owl-item.active').first().index();
+    var end = sync18.find('.owl-item.active').last().index();
+    
+    if (current > end) {
+      sync18.data('owl.carousel').to(current, 100, true);
+    }
+    if (current < start) {
+      sync18.data('owl.carousel').to(current - onscreen, 100, true);
+    }
+  }
+  
+  function syncPosition2(el) {
+    if(syncedSecondary) {
+      var number = el.item.index;
+      sync17.data('owl.carousel').to(number, 100, true);
+    }
+  }
+  
+  sync18.on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).index();
+    sync17.data('owl.carousel').to(number, 300, true);
+  });
+});
